d69771cf6766c0bf4991ef533a8a0eb57b775527
Markus Ölhafen <oelhafenmarkus@gmail.com>
2020-04-16 12:16:10 +0200

    add console logs

--- bin/polish-svg.js
+++ bin/polish-svg.js
@@ -5,39 +5,43 @@ import { format } from 'prettier';
 import DEFAULT_ATTRS from '../src/default-attrs.json';
 
 function polishSvg(svg) {
-    return (
-        optimize(svg)
-            .then(setAttrs)
-            .then(format)
-            // remove semicolon inserted by prettier
-            // because prettier thinks it's formatting JSX not HTML
-            .then(svg => svg.replace(/;/g, ''))
-    );
+  return (
+    optimize(svg)
+    .then(setAttrs)
+    .then(format)
+    // remove semicolon inserted by prettier
+    // because prettier thinks it's formatting JSX not HTML
+    .then(svg => svg.replace(/;/g, ''))
+  );
 }
 
 function optimize(svg) {
-    const svgo = new Svgo({
-        plugins: [
-            { convertShapeToPath: false },
-            { mergePaths: false },
-            { removeAttrs: { attrs: '(fill|stroke.*)' } },
-            { removeTitle: true },
-        ],
-    });
-  
-    return new Promise(resolve => {
-        svgo.optimize(svg, ({ data }) => resolve(data));
-    });
+  const svgo = new Svgo({
+    plugins: [
+      { convertShapeToPath: false },
+      { mergePaths: false },
+      { removeAttrs: { attrs: '(fill|stroke.*)' } },
+      { removeTitle: true },
+    ],
+  });
+
+  return new Promise(resolve => {
+    svgo.optimize(svg, ({ data }) => resolve(data));
+    
+    console.log('optimized...')
+  });
 }
 
 function setAttrs(svg) {
-    const $ = cheerio.load(svg);
   
-    Object.keys(DEFAULT_ATTRS).forEach(key =>
-        $('svg').attr(key, DEFAULT_ATTRS[key]),
-    );
+  console.log('setting Attributes...')
   
-    return $('body').html();
+  const $ = cheerio.load(svg);
+  Object.keys(DEFAULT_ATTRS).forEach(key =>
+    $('svg').attr(key, DEFAULT_ATTRS[key]),
+  );
+
+  return $('body').html();
 }
 
 export default polishSvg;
