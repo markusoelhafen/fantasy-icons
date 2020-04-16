ebab85a277e6e300411072448db26d4436dac20b
Markus Ölhafen <oelhafenmarkus@gmail.com>
2020-04-16 11:45:00 +0200

    add babelrc

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Process SVG files
 npx babel-node bin/polish-svgs.js
 
 # Create dist directory
-# npx rimraf dist
-# mkdir dist
+npx rimraf dist
+mkdir dist
 
 # build icons.json
 # npx babel-node bin/build-icons-json.js
